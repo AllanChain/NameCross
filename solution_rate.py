83cbf589901f1181a8f22b62ebb5fc82f8e48e98
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2019-07-07 11:12:05 +0800

    enhance rate printing

--- solution_rate.py
+++ solution_rate.py
@@ -1,7 +1,8 @@
 from main import NameMap
 from os import listdir
 
-for i in listdir('better'):
-    name_map = NameMap(seed='better\\'+i)
-    print(i, name_map.chr_total, name_map.border)
+name_maps = [NameMap(seed='better\\'+i) for i in listdir('better')]
+name_maps.sort(key=lambda m: 2*m.border+m.chr_total)
+for name_map in name_maps:
+    print(name_map.text_plain(), name_map.border, name_map.chr_total)
 
